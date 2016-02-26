8fa66e7ec9b31ed4ec672f35eae8a54e664a7104
NoemiaFranciscaGuerra <noemiaguerracx@gmail.com>
2016-02-26 15:53:03 -0300

    banco

---
+++ Agenda/app/src/main/java/com/example/noemia/agenda/Banco.java
@@ -0,0 +1,52 @@
+package com.example.noemia.agenda;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Noemia on 15/02/2016.
+ */
+  public class Banco extends SQLiteOpenHelper {
+
+    private SQLiteDatabase db;
+
+
+    public Banco(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE contato(_id INTEGER PRIMARY KEY  AUTOINCREMENT,nome TEXT, mumero TEXT, imagem integer)";
+        db.execSQL(sql);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = "DROP TABLE IF EXISTS contato";
+        db.execSQL(sql);
+
+    }
+
+    public Cursor buscar(String sql) {
+        return getWritableDatabase().rawQuery(sql, null);
+    }
+
+    public int updateLanche(Contato contato) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("nome", contato.getNome());
+        values.put("numero", contato.getNumero());
+        // updating row
+        return db.update("contatos", values, "_id = ?", new String[]{String.valueOf(contato.getId())});
+    }
+
+    public void deleteLanche(Contato contato) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete("contato", "_id = ?", new String[]{String.valueOf(contato.getId())});
+        db.close();
+    }
+}
